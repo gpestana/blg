3c5c3f0eb41ea01be83468c681b2c7f23d37016a
Pestana Gonçalo <pestang1@niksula.hut.fi>
2013-10-10 14:21:05 +0300

    postgres added

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+#database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 from views import general
