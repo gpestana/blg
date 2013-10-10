98ff0e0cd199836dd73c4659b85083e86fb4d50f
Pestana Gonçalo <pestang1@niksula.hut.fi>
2013-10-10 16:07:53 +0300

    postgres database working and access guaranteed

--- app/__init__.py
+++ app/__init__.py
@@ -8,9 +8,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', \
 	'postgresql+psycopg2://user:pass@localhost/test')
 db = SQLAlchemy(app)
 
-#Locally DB:
-#dropdb -U <user> <namedb>
-#createdb -U <user> <namedb> 
-
 from app import models
 from views import general
