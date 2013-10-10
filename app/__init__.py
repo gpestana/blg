8e9fca41b3128ecb3fb5c8acb0963e899baba7f3
Pestana Gonçalo <pestang1@niksula.hut.fi>
2013-10-10 14:35:25 +0300

    postgres fixed locally and remotely

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,14 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 #database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+#Heroku
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', \
+	'postgresql+psycopg2://user:pass@localhost/test')
+#Locally
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ[local]
+
+
 db = SQLAlchemy(app)
 
 from views import general
