016ad03ad60eea3519ebb2ec654818f7aa953668
Pestana Gonçalo <pestang1@niksula.hut.fi>
2013-11-05 01:43:49 +0200

    menu sketching

--- app/__init__.py
+++ app/__init__.py
@@ -14,13 +14,14 @@ app.secret_key = 'secret_key'
 
 #SQLAlchemy+db
 
-'''#Locally
+#Locally
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",\
 	"postgresql+psycopg2://user:pass@localhost/blg")
-'''
+
 
 #Heroku
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+
 db = SQLAlchemy(app)
 
 #Models+Views
